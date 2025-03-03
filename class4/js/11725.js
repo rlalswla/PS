ae918e274b1cbef71bc24a6e95a2b06be8edd28a
elect10 <wlgnsakswp@naver.com>
2025-03-04 02:58:23 +0900

    solve(js): 11725

--- class4/js/11725.js
+++ class4/js/11725.js
@@ -2,7 +2,26 @@ const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
 const solution = () => {
-  console.log(input);
+  const N = Number(input[0]);
+  const graph = Array.from({ length: N + 1 }, () => []);
+  const parent = Array(N + 1).fill(-1);
+  for (let i = 1; i < N; i++) {
+    const [a, b] = input[i].split(" ").map(Number);
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+  dfs(1, graph, parent);
+
+  parent.slice(2).forEach((input) => console.log(input));
+};
+
+const dfs = (index, graph, parent) => {
+  graph[index].forEach((element) => {
+    if (parent[element] === -1) {
+      parent[element] = index;
+      dfs(element, graph, parent);
+    }
+  });
 };
 
 solution();
